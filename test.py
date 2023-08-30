ef88bc163aabfe6d2b879beac4083568718035b9
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-30 17:05:44 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,45 +1,31 @@
-import pandas as pd
-import numpy as np
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.models import load_model
+import joblib
 
-# Load your dataset
-df = pd.read_csv('generated_dataset.csv')
+# Load the trained model
+model = joblib.load('model.joblib')
 
-# Load your trained Keras model
-model = load_model('model_revised.h5')
+# Load the label encoder used during training
+label_encoder = joblib.load('label_encoder.joblib')  # If you saved it during training
 
 
-# Define a function to recommend a topic for a user
-def recommend_topic(model, user_age, user_gender):
-    # Ensure user_age is an integer
-    user_age_input = np.array([[user_age]], dtype=int)
+# Function to recommend a topic based on user's age and gender
+def recommend_topic(user_age, user_gender):
+    # Predict the liked topic for the user
+    predicted_topic = model.predict([[user_age, user_gender]])
 
-    # Ensure user_gender is encoded as 0 for male and 1 for female
-    user_gender_input = np.array([[0 if user_gender == "M" else 1]], dtype=int)
+    # Convert the predicted label back to the original category
+    predicted_topic = label_encoder.inverse_transform(predicted_topic)
 
-    # Get all unique topics from the dataset
-    topics = df['LikedTopic'].unique()
-
-    # Ensure the number of topics matches the model's input shape (50 topics)
-    num_topics = 50  # Update this to match your model's input shape
-    dummy_topics = np.zeros((1, num_topics))  # Placeholder for topics data
-
-    # Use the model to predict topic scores
-    topic_scores = model.predict([user_age_input, user_gender_input, dummy_topics])
-
-    # Get the recommended topic index based on the highest score
-    recommended_topic_index = np.argmax(topic_scores)
-    recommended_topic_data = topics[recommended_topic_index]
-
-    return recommended_topic_data
+    return predicted_topic[0] if predicted_topic else 'Unknown'
 
 
-# Example usage:
-age_input = input("Enter user's age: ")
-gender_input = input("Enter user's gender (M/F): ")
+# Get user input for age and gender
+user_age = int(input("Enter your age: "))
+user_gender = input("Enter your gender (M/F): ")
 
-recommended_topic = recommend_topic(model, age_input, gender_input)
+# Encode the gender input (assuming label_encoder was used during training)
+user_gender = label_encoder.transform([user_gender])
 
-print(f"Recommended topic for user: {recommended_topic}")
+# Make a recommendation
+recommended_topic = recommend_topic(user_age, user_gender)
+
+print(f"Recommended Topic for User: {recommended_topic}")
