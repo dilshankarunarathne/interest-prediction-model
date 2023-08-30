534f95a055c735f379dfe84bdbc7f02bbdea4071
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-30 17:48:24 +0530

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my-ai-package",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+)
