53a83e437d613ae614436bf7f39ecc3089e4679d
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-30 17:32:02 +0530

    Update create_label_encoder.py

--- create_label_encoder.py
+++ create_label_encoder.py
@@ -1,16 +1,13 @@
 from sklearn.preprocessing import LabelEncoder
-
-# Read the unique topics from the text file
-with open('unique_topics.txt', 'r') as file:
-    unique_topics = [line.strip() for line in file.readlines()]
-
-# Create the LabelEncoder and fit it to the unique topics
-label_encoder = LabelEncoder()
-label_encoder.fit(unique_topics)
-
-# Save the label_encoder using joblib
 import joblib
-joblib.dump(label_encoder, 'label_encoder.joblib')
 
-print("LabelEncoder created and saved.")
-# TODO - this code actually wrecks the encoder saved during training
+lines = []
+
+with open("unique_topics.txt", "r") as topics:
+	lines = topics.readlines()
+
+label_encoder = LabelEncoder()
+label_encoder.classes_ = lines
+
+# save the label encoder
+joblib.dump(label_encoder, "label_encoder.joblib")
