cc0dcda9a728371d4a3a291329d6a74b6f354eed
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-30 16:39:01 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.layers import Input, Embedding, Flatten, Concatenate, Dense, LSTM
+from tensorflow.keras.layers import Input, Embedding, Concatenate, Flatten, Dense
 from tensorflow.keras.models import Model
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.utils import to_categorical
 
 # Load your dataset
 df = pd.read_csv('generated_dataset.csv')
@@ -23,7 +24,7 @@ topics_pad = pad_sequences(topics_seq, maxlen=max_topics_length)
 X_age = df['UserAge'].values
 X_gender = df['UserGender'].values
 X_topics = topics_pad
-y = np.ones(len(df))  # Binary label: 1 for 'liked', assuming all entries are liked
+y = to_categorical(df['TopicClass'])  # Assuming you have a 'TopicClass' column
 
 # Split the data into train and test sets
 X_age_train, X_age_test, X_gender_train, X_gender_test, X_topics_train, X_topics_test, y_train, y_test = train_test_split(
@@ -34,16 +35,21 @@ input_age = Input(shape=(1,), name='age_input')
 input_gender = Input(shape=(1,), name='gender_input')
 input_topics = Input(shape=(max_topics_length,), name='topics_input')
 
+embedding_age = Embedding(input_dim=100, output_dim=16)(input_age)  # Adjust input_dim as needed
+embedding_gender = Embedding(input_dim=2, output_dim=16)(input_gender)
 embedding_topics = Embedding(input_dim=len(tokenizer.word_index) + 1, output_dim=16)(input_topics)
-lstm_topics = LSTM(16)(embedding_topics)
 
-concatenated = Concatenate()([input_age, input_gender, lstm_topics])
-output = Dense(1, activation='sigmoid')(concatenated)
+lstm_age = Flatten()(embedding_age)
+lstm_gender = Flatten()(embedding_gender)
+lstm_topics = Flatten()(embedding_topics)
+
+concatenated = Concatenate()([lstm_age, lstm_gender, lstm_topics])
+output = Dense(len(tokenizer.word_index) + 1, activation='softmax')(concatenated)
 
 model = Model(inputs=[input_age, input_gender, input_topics], outputs=output)
 
 # Compile the model
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Train the model
 model.fit(
@@ -61,14 +67,5 @@ loss, accuracy = model.evaluate(
 )
 print(f'Test loss: {loss:.4f}, Test accuracy: {accuracy:.4f}')
 
-# Make recommendations for a new user (adjust input_data accordingly)
-input_data = {
-    'age_input': np.array([30]),
-    'gender_input': np.array([0]),  # 0 for Male, 1 for Female
-    'topics_input': np.array([topics_seq[0]])  # Replace with a user's liked topics
-}
-recommendation = model.predict(input_data)
-print(f'Recommendation: {recommendation[0][0]}')
-
 # Save the model
-model.save('model_revised.h5')
+model.save('topic_recommendation_model.h5')
