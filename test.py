8cb9aec6cbe88c93782e429fa7a3d7d0e1cc2f2d
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-30 16:07:58 +0530

    Update test.py

--- test.py
+++ test.py
@@ -11,40 +11,31 @@ df = pd.read_csv('generated_dataset.csv')
 model = load_model('model.h5')
 
 
-# Define a function to recommend topics based on user input
-def recommend_topics(user_age, user_gender):
-    # Ensure user age is numeric
-    user_age = pd.to_numeric(user_age, errors='coerce')
+# Define a function to recommend topics for a user
+def recommend_topic(model, user_age, user_gender):
+    # Create input data for the model
+    user_age_input = np.array([[user_age]])
+    user_gender_input = np.array([[user_gender]])
 
-    # Encode user gender (assuming 'M' is 0 and 'F' is 1)
-    user_gender = 0 if user_gender == 'M' else 1
-
-    # Preprocess the data for prediction
-    max_topics_length = 50
-    tokenizer = Tokenizer()
-    tokenizer.fit_on_texts(df['LikedTopic'])
-
-    # Create a dummy sequence for topics (you can modify this based on user input)
-    dummy_topic_sequence = tokenizer.texts_to_sequences(['your_dummy_topic'])
-    dummy_topic_padded = pad_sequences(dummy_topic_sequence, maxlen=max_topics_length)
+    # Generate dummy topics data (you can replace this with actual topics)
+    num_topics = 50  # Adjust this based on your dataset
+    dummy_topics = np.zeros((1, num_topics))  # Placeholder for topics data
 
     # Use the model to predict topic scores
-    predicted_scores = model.predict([[user_age], [user_gender], dummy_topic_padded])
+    topic_scores = model.predict([user_age_input, user_gender_input, dummy_topics])
 
-    # Define a threshold for recommendations (adjust as needed)
-    threshold = 0.5
+    # Assuming you have a list of topics, you can select the topic with the highest score
+    topics = ["Topic 1", "Topic 2", "Topic 3", ...]  # Replace with your actual topics
+    recommended_topic_data = topics[np.argmax(topic_scores)]
 
-    # Get recommended topics based on the threshold
-    recommended_topics_list = [topic for i, topic in enumerate(df['LikedTopic']) if predicted_scores[i][0] >= threshold]
-
-    return recommended_topics_list
+    return recommended_topic_data
 
 
 # Example usage:
 user_age_input = input("Enter user's age: ")
 user_gender_input = input("Enter user's gender (M/F): ")
 
-recommended_topics = recommend_topics(user_age_input, user_gender_input)
+recommended_topics = recommend_topic(model, user_age_input, user_gender_input)
 
 if recommended_topics:
     print("Recommended Topics:")
