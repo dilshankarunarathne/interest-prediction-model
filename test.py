7c1370cac6b0fd153b34c7b352a671c57fb7f515
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-30 17:26:05 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,21 @@
 import joblib
 import os
+import warnings
 
 # Load the trained model
 model = joblib.load('model.joblib')
 
 # Try to load the label encoder, or use a default one if not found
 label_encoder_path = 'label_encoder.joblib'
-label_encoder = joblib.load(label_encoder_path)
+if os.path.exists(label_encoder_path):
+    label_encoder = joblib.load(label_encoder_path)
+else:
+    # Create a default label encoder (assuming you know the classes in advance)
+    from sklearn.preprocessing import LabelEncoder
+
+    label_encoder = LabelEncoder()
+    label_encoder.classes_ = ['art', 'books', 'business', 'fashion', 'food', 'movies', 'music', 'science', 'sports',
+                              'technology', 'travel', 'gaming']
 
 
 # Function to recommend a topic based on user's age and gender
@@ -14,13 +23,20 @@ def recommend_topic(user_age, user_gender):
     # Encode the gender input (assuming label_encoder was used during training)
     user_gender_encoded = label_encoder.transform([user_gender])
 
-    # Predict the liked topic for the user
-    predicted_topic = model.predict([[user_age, user_gender_encoded[0]]])
+    # Suppress the UserWarning
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
 
-    # Convert the predicted label back to the original category
-    predicted_topic = label_encoder.inverse_transform(predicted_topic)
+        # Predict the liked topic for the user by passing input as a dictionary
+        predicted_topic = model.predict({"UserAge": [user_age], "UserGender": [user_gender_encoded[0]]})
 
-    return predicted_topic[0] if predicted_topic else 'Unknown'
+    # Convert the predicted label back to the original category, or use the predicted label directly
+    if predicted_topic[0] in label_encoder.classes_:
+        predicted_topic = label_encoder.inverse_transform(predicted_topic)
+    else:
+        predicted_topic = predicted_topic[0]
+
+    return predicted_topic
 
 
 # Get user input for age and gender
