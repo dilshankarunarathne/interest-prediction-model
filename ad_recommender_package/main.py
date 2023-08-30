760a58b740a7439eb4504f861dae905d6539cbed
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-30 18:01:03 +0530

    Update main.py

--- ad_recommender_package/main.py
+++ ad_recommender_package/main.py
@@ -10,10 +10,9 @@ model = joblib.load(model_file_path)
 label_encoder = joblib.load(label_encoder_file_path)
 
 
-# Function to recommend a topic based on user's age and gender
-def recommend_topic(user_age, user_gender):
+def recommend_topic(user_age, gender):
     # Encode the gender input (assuming label_encoder was used during training)
-    user_gender_encoded = label_encoder.transform([user_gender])
+    user_gender_encoded = label_encoder.transform([gender])
 
     # Predict the liked topic for the user by passing input as a NumPy array
     input_data = np.array([[user_age, user_gender_encoded[0]]], dtype=float)
