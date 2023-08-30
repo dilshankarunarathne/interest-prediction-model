3dfbfad97bf526c25c0410237f33299f6a08260f
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-30 16:43:12 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -2,19 +2,17 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from tensorflow.keras.layers import Input, Embedding, Concatenate, Flatten, Dense
-from tensorflow.keras.models import Model
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Dense
 from tensorflow.keras.utils import to_categorical
 
 # Load your dataset
 df = pd.read_csv('generated_dataset.csv')
 
-# Create a label encoder
-label_encoder = LabelEncoder()
-
 # Encode the 'LikedTopic' column into numerical labels
+label_encoder = LabelEncoder()
 df['TopicClass'] = label_encoder.fit_transform(df['LikedTopic'])
 
 # Tokenize and pad liked topics
@@ -28,48 +26,29 @@ topics_pad = pad_sequences(topics_seq, maxlen=max_topics_length)
 X_age = df['UserAge'].values
 X_gender = df['UserGender'].values
 X_topics = topics_pad
-y = to_categorical(df['LikedTopic'])
+y = to_categorical(df['TopicClass'])  # Use the 'TopicClass' column as labels
 
 # Split the data into train and test sets
 X_age_train, X_age_test, X_gender_train, X_gender_test, X_topics_train, X_topics_test, y_train, y_test = train_test_split(
     X_age, X_gender, X_topics, y, test_size=0.2, random_state=42)
 
 # Define the model
-input_age = Input(shape=(1,), name='age_input')
-input_gender = Input(shape=(1,), name='gender_input')
-input_topics = Input(shape=(max_topics_length,), name='topics_input')
-
-embedding_age = Embedding(input_dim=100, output_dim=16)(input_age)  # Adjust input_dim as needed
-embedding_gender = Embedding(input_dim=2, output_dim=16)(input_gender)
-embedding_topics = Embedding(input_dim=len(tokenizer.word_index) + 1, output_dim=16)(input_topics)
-
-lstm_age = Flatten()(embedding_age)
-lstm_gender = Flatten()(embedding_gender)
-lstm_topics = Flatten()(embedding_topics)
-
-concatenated = Concatenate()([lstm_age, lstm_gender, lstm_topics])
-output = Dense(len(tokenizer.word_index) + 1, activation='softmax')(concatenated)
-
-model = Model(inputs=[input_age, input_gender, input_topics], outputs=output)
+model = Sequential()
+model.add(Embedding(input_dim=len(tokenizer.word_index) + 1, output_dim=16, input_length=max_topics_length))
+model.add(LSTM(16))
+model.add(Dense(len(label_encoder.classes_), activation='softmax'))
 
 # Compile the model
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Train the model
-model.fit(
-    {'age_input': X_age_train, 'gender_input': X_gender_train, 'topics_input': X_topics_train},
-    y_train,
-    epochs=10,
-    batch_size=64,
-    validation_split=0.2
-)
+model.fit({'age_input': X_age_train, 'gender_input': X_gender_train, 'topics_input': X_topics_train},
+          y_train, epochs=10, batch_size=64, validation_split=0.2)
 
 # Evaluate the model
-loss, accuracy = model.evaluate(
-    {'age_input': X_age_test, 'gender_input': X_gender_test, 'topics_input': X_topics_test},
-    y_test
-)
+loss, accuracy = model.evaluate({'age_input': X_age_test, 'gender_input': X_gender_test, 'topics_input': X_topics_test},
+                                y_test)
 print(f'Test loss: {loss:.4f}, Test accuracy: {accuracy:.4f}')
 
 # Save the model
-model.save('topic_recommendation_model.h5')
+model.save('model_2.h5')
