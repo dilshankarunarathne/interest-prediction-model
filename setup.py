14a23ce495f964dfaa296eb8c6a5ecc14ca35acd
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-30 17:53:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="my-ai-package",
+    name="ad_topic_recommender",
     version="0.1",
     packages=find_packages(),
     install_requires=[
