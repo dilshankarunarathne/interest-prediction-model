ed015564b46d0a9fd7aa4463c22dc733e8c556db
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-30 18:01:09 +0530

    Update main.py

--- ad_recommender_package/main.py
+++ ad_recommender_package/main.py
@@ -10,12 +10,12 @@ model = joblib.load(model_file_path)
 label_encoder = joblib.load(label_encoder_file_path)
 
 
-def recommend_topic(user_age, gender):
+def recommend_topic(age, gender):
     # Encode the gender input (assuming label_encoder was used during training)
     user_gender_encoded = label_encoder.transform([gender])
 
     # Predict the liked topic for the user by passing input as a NumPy array
-    input_data = np.array([[user_age, user_gender_encoded[0]]], dtype=float)
+    input_data = np.array([[age, user_gender_encoded[0]]], dtype=float)
     with warnings.catch_warnings():  # Suppress warnings temporarily
         warnings.simplefilter("ignore")
         predicted_topic = model.predict(input_data)
