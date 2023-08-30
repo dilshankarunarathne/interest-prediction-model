5618f465bd0fcd0b4111d48c3fac0df56483d3be
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-30 17:27:02 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import joblib
 import os
 import warnings
+import numpy as np
 
 # Load the trained model
 model = joblib.load('model.joblib')
@@ -23,10 +24,11 @@ def recommend_topic(user_age, user_gender):
     # Encode the gender input (assuming label_encoder was used during training)
     user_gender_encoded = label_encoder.transform([user_gender])
 
-    # Predict the liked topic for the user by passing input as a dictionary
+    # Predict the liked topic for the user by passing input as a NumPy array
+    input_data = np.array([[user_age, user_gender_encoded[0]]], dtype=float)
     with warnings.catch_warnings():  # Suppress warnings temporarily
         warnings.simplefilter("ignore")
-        predicted_topic = model.predict({"UserAge": [user_age], "UserGender": [user_gender_encoded[0]]})
+        predicted_topic = model.predict(input_data)
 
     # Convert the predicted label back to the original category, or use the predicted label directly
     if predicted_topic[0] in label_encoder.classes_:
