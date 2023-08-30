3b565316abbbd37225a6b2207122454c0b439ec1
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-30 16:24:26 +0530

    Update test.py

--- test.py
+++ test.py
@@ -23,7 +23,7 @@ def recommend_topic(model, user_age, user_gender):
     topics = df['LikedTopic'].unique()
 
     # Ensure the number of topics matches the model's input shape (50 topics)
-    num_topics = 50
+    num_topics = 31
     dummy_topics = np.zeros((1, num_topics))  # Placeholder for topics data
 
     # Use the model to predict topic scores
